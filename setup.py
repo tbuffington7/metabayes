4bc56d49e19ed54636e7fbba332e19102f0178ba
tbuffington7 <tyler.c.buffington@gmail.com>
2024-02-29 00:44:26 -0600

    version bump

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'A tool for Bayesian meta analysis'
 LONG_DESCRIPTION = """
 A hierarchical model with random effects to understand aggregated effects of experiments, and to inform priors
